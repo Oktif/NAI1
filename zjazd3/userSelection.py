c9e7e96609737fe3e98170407f22e7014a52cb29
Oktif <47919676+Oktif@users.noreply.github.com>
2022-11-10 22:50:12 +0100

    V 0.88
    - changed userSelection to work even if new users are added
    - added ability to list all users

--- zjazd3/userSelection.py
+++ zjazd3/userSelection.py
@@ -1,59 +1,21 @@
+from ratingParse import parse
+userRatings = parse()
+'''
+Loop used for inputting desired user
+'''
 def user_name():
     u = None
-    user = None
-    while True:
-        user = input("Please type-in full name of User: ")
-        if user == 'Paweł Czapiewski':
-            u = 0
-            break
-        elif user == 'Łukasz Cettler':
-            u = 1
-            break
-        elif user == 'Paweł Iwiński':
-            u = 2
-            break
-        elif user == 'Oktawian Filipkowski':
-            u = 3
-            break
-        elif user == 'Krzysztof Lewandowski':
-            u = 4
-            break
-        elif user == 'Kamil Kornatowski':
-            u = 5
-            break
-        elif user == 'Dominik Pasymowski':
-            u = 6
-            break
-        elif user == 'Maciej Zakrzewski':
-            u = 7
-            break
-        elif user == 'Jakub Jabłoński':
-            u = 8
-            break
-        elif user == 'Stanisław Kibort':
-            u = 9
-            break
-        elif user == 'Marcin Kloczkowski':
-            u = 10
-            break
-        elif user == 'Wojciech Mierzejewski':
-            u = 11
-            break
-        elif user == 'Adam Kałuża':
-            u = 12
-            break
-        elif user == 'Cezary Graban':
-            u = 13
-            break
-        elif user == 'Sylwester Kąkol':
-            u = 14
-            break
-        elif user == 'Adam Jurkiewicz':
-            u = 15
-            break
-        elif user == 'Michał Cichowski':
-            u = 16
-            break
-    return u
-
-
+    while u == None:
+        user_inputted = input('Please type-in full name of the User: ')
+        for i in range(len(userRatings)):
+            user_1 = userRatings[i]
+            if user_inputted == user_1.name:
+                u = i
+                break
+            elif user_inputted.upper() == 'AMNESIA':
+                print(user_1.name)
+                continue
+        else:
+            print('incorrect data, please enter correct one, if you are unsure whats your name enter AMNESIA')
+            continue
+        return u
