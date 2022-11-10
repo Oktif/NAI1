e94a40469272927b9d391b4410adf827bbeed795
Oktif <47919676+Oktif@users.noreply.github.com>
2022-11-10 19:49:38 +0100

    V 0.85
    -Fixed screenshots
    -Added working function to chose user by his name
    -Code cleanup

--- zjazd3/main.py
+++ zjazd3/main.py
@@ -1,29 +1,26 @@
 '''
 Programming Team: Maciej Zakrzewski, Oktawian Filipkowski.
 Purpose of this program is to recommend movies to watch or to warn users against watching bad ones.
-We will start by importing liblaries
+We will start by importing libraries
 '''
 from ratingParse import parse
 from userRating import userRating
-
+from userSelection import user_name
 '''
 Then we will import parsed data
 '''
 userRatings = parse()
-
 '''
-Next user will provide program with his id
+Next we will ask user to provide us his full Name and Surname, to assign this program with his ID
 '''
-u = int(input('Please provide program with your identification number '))
+u = user_name()
 user = userRatings[u]
-print('You entered', u, 'that means your identification is', user.name)
-comparingList = user.ratings
-s = 0
-
 '''
 Now program will make advanced calculations using data provided with .csv file
 '''
-for i in range(17):
+comparingList = user.ratings
+s = 0
+for i in range(len(userRatings)):
     if i != u:
         user2 = userRatings[i]
         comparedList = user2.ratings
@@ -36,6 +33,7 @@ for i in range(17):
 
 '''
 After calculations user's soulmate will be found and movies will be reccomended for our dear user
+ps. In case of Mr.Czapiewski "Listy do M" are used as an option because there is typo in the name of the movie
 '''
 print('Congrats', user.name, 'your soulmate is', user3.name)
 print('Your average score is',user.getAverageScore(), 'your\'s soulmate\'s is', user3.getAverageScore(), 'quite a picky bunch, aren\'t ya?')
@@ -47,31 +45,21 @@ List4 = list(user.getMinRated(10))
 List5 = list(user3.getTopRated(10))
 List6 = list(user3.getMinRated(10))
 List7 = list()
-#for List5, List3 in zip(List5, List3):
-#    List7.append((List5, List3))
 List8 = list()
-#for List6, List4 in zip(List6, List4):
-#    List8.append((List6, List4))
-
-#print(List6)
-#print(List4)
 
 def in_list(list1,list2):
     for i in list1:
         if i not in list2:
-            List8.append(i)
+            List7.append(i)
     return True
 
 def in_list2(list1,list2):
     for i in list1:
         if i not in list2:
-            List7.append(i)
+            List8.append(i)
     return True
-in_list2(List5,List3)
-in_list(List6,List4)
-
-#result = list(set(List6).difference(List4))
-#print(result)
+in_list(List5,List3)
+in_list2(List6,List4)
 
 print('You should watch:', List7[:5])
 print('You shouldn\'t watch:', List8[:4:-1])
---
+++ zjazd3/userSelection.py
@@ -0,0 +1,59 @@
+def user_name():
+    u = None
+    user = None
+    while True:
+        user = input("Please type-in full name of User: ")
+        if user == 'Paweł Czapiewski':
+            u = 0
+            break
+        elif user == 'Łukasz Cettler':
+            u = 1
+            break
+        elif user == 'Paweł Iwiński':
+            u = 2
+            break
+        elif user == 'Oktawian Filipkowski':
+            u = 3
+            break
+        elif user == 'Krzysztof Lewandowski':
+            u = 4
+            break
+        elif user == 'Kamil Kornatowski':
+            u = 5
+            break
+        elif user == 'Dominik Pasymowski':
+            u = 6
+            break
+        elif user == 'Maciej Zakrzewski':
+            u = 7
+            break
+        elif user == 'Jakub Jabłoński':
+            u = 8
+            break
+        elif user == 'Stanisław Kibort':
+            u = 9
+            break
+        elif user == 'Marcin Kloczkowski':
+            u = 10
+            break
+        elif user == 'Wojciech Mierzejewski':
+            u = 11
+            break
+        elif user == 'Adam Kałuża':
+            u = 12
+            break
+        elif user == 'Cezary Graban':
+            u = 13
+            break
+        elif user == 'Sylwester Kąkol':
+            u = 14
+            break
+        elif user == 'Adam Jurkiewicz':
+            u = 15
+            break
+        elif user == 'Michał Cichowski':
+            u = 16
+            break
+    return u
+
+
